f585faa907919164e7f1b3712b23b0dbd63117b8
TemiArtemi <ts.suarez2006@gmail.com>
2026-04-09 11:08:03 +0100

    Begins the implementation of the API change. It creates the Team class.

---
+++ modulo-resultados/src/main/java/org/ulpgc/dacd/Team.java
@@ -0,0 +1,22 @@
+package org.ulpgc.dacd;
+
+public class Team {
+    private final int id;
+    private final String name;
+    private final String shortName;
+
+    public Team(int id, String name, String shortName) {
+        this.id = id;
+        this.name = name;
+        this.shortName = shortName;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getShortName() { return shortName; }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
