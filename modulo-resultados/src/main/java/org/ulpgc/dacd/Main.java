1f9dd2cbc2e2594c22fc419c5192dd782607f506
TemiArtemi <ts.suarez2006@gmail.com>
2026-04-09 19:50:39 +0100

    Creates the MatchController class and modify the Main method so that it calls the MatchController method.

--- modulo-resultados/src/main/java/org/ulpgc/dacd/Main.java
+++ modulo-resultados/src/main/java/org/ulpgc/dacd/Main.java
@@ -1,31 +1,19 @@
 package org.ulpgc.dacd;
 
-import org.ulpgc.dacd.Match;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 
         String apiKey = System.getenv("FOOTBALL_DATA_API_KEY");
-
         if (apiKey == null || apiKey.isEmpty()) {
             System.err.println("ERROR: No se ha encontrado la variable de entorno FOOTBALL_DATA_API_KEY.");
-            System.err.println("Por favor, configúrala en Run > Edit Configurations.");
             return;
         }
+        String dbUrl = "jdbc:sqlite:data/cuotas_deporte.db";
 
-        MatchParser parser = new MatchParser();
-        MatchFeeder feeder = new FootballDataOrgFeeder(apiKey, parser);
+        MatchFeeder feeder = new FootballDataOrgFeeder(apiKey, new MatchParser());
+        MatchStore store = new SqliteMatchStore(dbUrl);
 
-        System.out.println("Conectando con la API de La Liga...");
-        List<Match> matches = feeder.getMatches();
-
-        if (matches.isEmpty()) {
-            System.out.println("No se han encontrado partidos o ha ocurrido un error.");
-        } else {
-            System.out.println("¡Éxito! Se han capturado " + matches.size() + " partidos.");
-            System.out.println("Aquí tienes el primero de la lista:");
-            System.out.println(matches.getFirst().toString());
-        }
+        MatchController controller = new MatchController(feeder, store);
+        controller.execute();
     }
 }
---
+++ modulo-resultados/src/main/java/org/ulpgc/dacd/MatchController.java
@@ -0,0 +1,26 @@
+package org.ulpgc.dacd;
+
+import java.util.List;
+
+public class MatchController {
+
+    private final MatchFeeder feeder;
+    private final MatchStore store;
+
+    public MatchController(MatchFeeder feeder, MatchStore store) {
+        this.feeder = feeder;
+        this.store = store;
+    }
+
+    public void execute() {
+        System.out.println("Iniciando tarea de captura de resultados...");
+
+        List<Match> matches = feeder.getMatches();
+
+        if (!matches.isEmpty()) {
+            store.save(matches);
+        } else {
+            System.out.println("⚠️ No se obtuvieron datos nuevos para guardar.");
+        }
+    }
+}
