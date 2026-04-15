7587e00e929ebb36404bc145a7fa6879ef800a0d
TemiArtemi <ts.suarez2006@gmail.com>
2026-04-15 16:39:42 +0100

    Implements the API_KEY as an argument passed to the main method

--- modulo-resultados/src/main/java/org/ulpgc/dacd/Main.java
+++ modulo-resultados/src/main/java/org/ulpgc/dacd/Main.java
@@ -7,19 +7,23 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) {
 
-        String apiKey = System.getenv("FOOTBALL_DATA_API_KEY");
-        if (apiKey == null || apiKey.isEmpty()) {
-            System.err.println("ERROR: No se ha encontrado la variable de entorno FOOTBALL_DATA_API_KEY.");
+        if (args.length == 0) {
+            System.err.println("ERROR: No se ha proporcionado la API Key.");
+            System.err.println("Uso correcto: java -jar proyecto.jar <TU_API_KEY>");
             return;
         }
+
+        String apiKey = args[0];
         String dbUrl = "jdbc:sqlite:data/cuotas_deporte.db";
 
         MatchFeeder feeder = new FootballDataOrgFeeder(apiKey, new MatchParser());
         MatchStore store = new SqliteMatchStore(dbUrl);
         MatchController controller = new MatchController(feeder, store);
 
+        System.out.println("Iniciando servicio de captura de datos de Fútbol...");
+        System.out.println("El programa se ejecutará ahora mismo y se repetirá cada 6 horas.");
+
         ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
         scheduler.scheduleAtFixedRate(controller::execute, 0, 6, TimeUnit.HOURS);
-
     }
 }
